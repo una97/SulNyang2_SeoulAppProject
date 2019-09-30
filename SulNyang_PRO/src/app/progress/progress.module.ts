8afe84b7f9f5d827f85e38213c6f28ce61e7bacf
Soyoung Yoo <yooso020599@naver.com>
2019-09-30 13:38:18 +0900

    name change

--- SulNyang_PRO/src/app/progress/progress.module.ts
+++ SulNyang_PRO/src/app/progress/progress.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+import {ChartsModule} from 'ng2-charts';
 import { IonicModule } from '@ionic/angular';
 
 import { ProgressPage } from './progress.page';
@@ -19,6 +19,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ChartsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [ProgressPage]
